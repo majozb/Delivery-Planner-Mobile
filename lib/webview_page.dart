45028171c4cebb257d7bb6313d1f21954223f597
majozb <alu0101516608@ull.edu.es>
2025-05-05 21:53:56 +0100

    feat: initial configuration

---
+++ lib/webview_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebviewPage extends StatefulWidget {
+  const WebviewPage({Key? key}) : super(key: key);
+
+  @override
+  State<WebviewPage> createState() => _WebviewPageState();
+}
+
+class _WebviewPageState extends State<WebviewPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final PlatformWebViewControllerCreationParams params =
+        const PlatformWebViewControllerCreationParams();
+
+    _controller = WebViewController.fromPlatformCreationParams(params)
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {
+            print("Loading: $url");
+          },
+          onPageFinished: (String url) {
+            print("Loaded: $url");
+          },
+          onWebResourceError: (WebResourceError error) {
+            print("Error: ${error.description}");
+          },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('http://localhost:3000/'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Delivery Planner App"),
+      ),
+      body: WebViewWidget(controller: _controller),
+);
+}
+}
